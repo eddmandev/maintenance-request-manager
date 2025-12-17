847264fc5ad69ca64d91f1de4f70c145995eca3a
edd man <kadixeee@gmail.com>
2025-12-17 14:35:11 +0100

    Initial commit

---
+++ src/main/java/wit/edu/inz/MaintenanceRequestManagerApplication.java
@@ -0,0 +1,13 @@
+package wit.edu.inz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceRequestManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceRequestManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wit/edu/inz/MaintenanceRequestManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package wit.edu.inz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceRequestManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
